c1b5d29ea8ed18a35bc5d1750ad171b923918df9
NomadikLiving <marcus@nomadikliving.com>
2024-02-14 16:56:15 -0700

    remove features2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Header from "@/components/Header";
 import Contact from "@/components/index/Contact";
 import Features from "@/components/index/Features";
-import Features2 from "@/components/index/Features2";
 import Hero from "@/components/index/Hero";
 import Table from "@/components/index/Table";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,7 +11,6 @@ export default function Home() {
       <main>
         <Hero />
         <Features />
-        {/* <Features2 /> */}
         <Table />
         <Contact />
       </main>
