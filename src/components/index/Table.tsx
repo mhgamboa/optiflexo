123d3918634000f7299304929e4cf8234503799b
NomadikLiving <marcus@nomadikliving.com>
2024-02-14 16:56:58 -0700

    begin table

--- src/components/index/Table.tsx
+++ src/components/index/Table.tsx
@@ -1,43 +1,118 @@
 import React from "react";
 
 export default function Table() {
+  // Data for the 2nd and 3rd rows
+  const rowData = [
+    {
+      lineNumber: "55",
+      values: ["15.0", "50.0", "15.0", "50.0", "18.0", "50.0", "18.0", "50.0"],
+    },
+    {
+      lineNumber: "80",
+      values: ["10.0", "33.0", "10.0", "33.0", "15.0", "33.0", "15.0", "33.0"],
+    },
+    {
+      lineNumber: "100",
+      values: ["9.0", "25.0", "9.0", "25.0", "14.0", "25.0", "14.0", "25.0"],
+    },
+    {
+      lineNumber: "120",
+      values: ["8.0", "22.0", "8.0", "22.0", "12.2", "22.0", "12.2", "22.0"],
+    },
+    {
+      lineNumber: "160",
+      values: ["6.0", "16.0", "6.0", "16.0", "9.0", "16.0", "9.0", "16.0"],
+    },
+    {
+      lineNumber: "180",
+      values: ["5.4", "16.0", "5.4", "16.0", "6.2", "16.0", "6.2", "16.0"],
+    },
+    {
+      lineNumber: "200",
+      values: ["5.2", "14.0", "5.2", "14.0", "5.2", "14.0", "5.2", "14.0"],
+    },
+    {
+      lineNumber: "260",
+      values: ["3.8", "12.0", "3.8", "12.0", "4.7", "12.0", "4.7", "12.0"],
+    },
+    {
+      lineNumber: "300",
+      values: ["3.5", "10.0", "3.5", "10.0", "3.5", "6.5", "3.5", "6.5"],
+    },
+    {
+      lineNumber: "360",
+      values: ["2.8", "7.5", "2.8", "7.5", "3.7", "7.5", "3.5", "6.5"],
+    },
+    {
+      lineNumber: "400",
+      values: ["2.0", "6.5", "2.8", "6.5", "3.5", "6.5", "3.5", "6.5"],
+    },
+    {
+      lineNumber: "440",
+      values: ["2.0", "6.0", "2.7", "6.0", "3.2", "6.0", "", ""],
+    },
+  ];
+
   return (
     <div className="overflow-scroll sm:overflow-x-auto p-10 max-x-full">
-      <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+      <h2 className="text-3xl font-bold sm:text-4xl text-center pb-6">Grabados Anilox</h2>
+
+      <table className="mx-auto divide-y-2 divide-gray-200 bg-white text-sm">
         <thead className="text-left">
-          <tr>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Name</th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              Date of Birth
+          <tr className="">
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"></th>
+            <th
+              className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900 text-center"
+              colSpan={2}
+            >
+              OPTI 30/60
             </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Role</th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Salary</th>
+            <th
+              className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900 text-center"
+              colSpan={2}
+            >
+              OPTI Q
+            </th>
+            <th
+              className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900 text-center"
+              colSpan={2}
+            >
+              OPTI T
+            </th>
+            <th
+              className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900 text-center"
+              colSpan={2}
+            >
+              OPTI X
+            </th>
           </tr>
         </thead>
 
         <tbody className="divide-y divide-gray-200">
-          <tr className="odd:bg-gray-50">
-            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">John Doe</td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
+          <tr className="odd:bg-gray-100 ">
+            <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Lìneas</th>
+            <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Min.</th>
+            <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Max.</th>
+            <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Min.</th>
+            <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Max.</th>
+            <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Min.</th>
+            <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Max.</th>
+            <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Min.</th>
+            <th className="whitespace-nowrap px-4 py-2 font-semibold text-gray-900">Max.</th>
           </tr>
 
-          <tr className="odd:bg-gray-50">
-            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Jane Doe</td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">04/11/1980</td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Designer</td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">$100,000</td>
-          </tr>
-
-          <tr className="odd:bg-gray-50">
-            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              Gary Barlow
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">Singer</td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">$20,000</td>
-          </tr>
+          {rowData.map((row, index) => (
+            <tr key={index} className="odd:bg-gray-100 ">
+              <td className="whitespace-nowrap px-4 py-2 font-semibold text-black">
+                {row.lineNumber}
+              </td>
+              {row.values.map((value, idx) => (
+                <td key={idx} className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  {value}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
