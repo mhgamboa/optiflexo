4248e8a3f0c07c311c2426d6ed6c84eeee5a8ca3
NomadikLiving <marcus@nomadikliving.com>
2024-02-15 09:02:21 -0700

    Update language for contact form

--- src/components/index/Contact.tsx
+++ src/components/index/Contact.tsx
@@ -5,53 +5,53 @@ export default function Contact() {
     <section className="bg-rose-700" id="contactar">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">
-          Contact Us
+          Contactarnos
         </h2>
         <p className="mb-8 lg:mb-16 font-light text-center text-white sm:text-xl">
-          Got a technical issue? Want to send feedback about a beta feature? Need details about
-          our Business plan? Let us know.
+          {/* à, è, ì, ò, ù, À, È, Ì, Ò, Ù */}
+          ¿Còmo le podemos ayudar? Contàctenos hoy, y le mandamos un email.
         </p>
         <form action="#" className="space-y-8">
           <div>
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">
-              Your email
+              Su Email
             </label>
             <input
               type="email"
               id="email"
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 placeholder-gray-400 focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
-              placeholder="name@flowbite.com"
+              placeholder="nombre@email.com"
               required
             />
           </div>
           <div>
             <label htmlFor="subject" className="block mb-2 text-sm font-medium text-white">
-              Subject
+              Subjeto
             </label>
             <input
               type="text"
               id="subject"
               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 placeholder-gray-400 focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
-              placeholder="Let us know how we can help you"
+              placeholder="Diganos como le podemos ayudar"
               required
             />
           </div>
           <div className="sm:col-span-2">
             <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">
-              Your message
+              Su Mensaje
             </label>
             <textarea
               id="message"
               rows={6}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 placeholder-gray-400 focus:ring-primary-500 focus:border-primary-500"
-              placeholder="Leave a comment..."
+              placeholder="Ponga su mensaje aquì..."
             ></textarea>
           </div>
           <button
             type="submit"
             className="py-3 px-5 text-sm font-semibold text-center bg-white text-rose-600 rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 focus:ring-primary-800"
           >
-            Send message
+            Manda Un Mensaje
           </button>
         </form>
       </div>
