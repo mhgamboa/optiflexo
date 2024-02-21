30bd037bcc76c8c37797ae2d5dc2f114d206c4d8
NomadikLiving <marcus@nomadikliving.com>
2024-02-21 11:59:19 -0700

    Update Paragraph.tsx

--- src/components/index/Paragraph.tsx
+++ src/components/index/Paragraph.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Paragraph() {
   return (
     <div className="w-full font-semibold flex items-center justify-center py-16 md:py-24 lg:py-36">
-      <h1 className="text-xl lg:text-2xl leading-10 text-rose-600 text-center w-4/5 max-w-screen-xl">
+      <h1 className="text-xl lg:text-2xl xl:text-3xl text-rose-700 w-4/5 max-w-screen-md">
         Estamos comprometidos a entregar productos a nuestros clientes a tiempo y de manera
         confiable. Sólo a través de credibilidad calidad y la mejora continua podremos alcanzar
         juntos nuestros objetivos y garantizar el éxito a largo plazo.
