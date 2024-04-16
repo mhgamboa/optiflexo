ed01480652cee0eba891b0aaab92b3c01c835c05
NomadikLiving <marcus@nomadikliving.com>
2024-04-16 11:13:25 -0600

    Update layout for /en.
    Must have English metadata

---
+++ src/app/en/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Optiflexo",
+  description: "The Optimal option for Anilox Rollers and Sleeves for Narrow and Medium Bands",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body className={inter.className}>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
