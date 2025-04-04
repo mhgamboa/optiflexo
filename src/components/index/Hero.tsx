059919e23c5e7de53745e4095732ce9d5c4edb23
Marcus Gamboa <marcus.gamboa@gmail.com>
2025-04-04 12:26:08 -0600

    Update Hero.tsx
    Change white gradient from 75 to 95

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -11,10 +11,8 @@ export default function Hero({ lang }: { lang?: string }) {
     backgroundRepeat: "no-repeat",
   };
   return (
-    // <section className="relative bg-[url(/roller.jpg)] bg-cover bg-center bg-no-repeat">
     <section className="relative" style={sectionStyle}>
-      {/* formerly sm:from-white/95 sm:to-white/25 */}
-      <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/65 sm:to-white/25 sm:bg-gradient-to-r" />
+      <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 sm:bg-gradient-to-r" />
 
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:py-0 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
         <div className="max-w-xl text-center sm:text-left mx-auto sm:mx-0 space-y-6">
